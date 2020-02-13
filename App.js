3a23144457a7c0cbae46c3fc27e27b1e52aaed14
전석인 <andy-chun@jeonseog-in-ui-MacBookPro.local>
2020-02-13 11:54:05 +0900

    commit all

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
-export default function App() {
-  return <Loading />;
+
+export default class extends React.Component {
+
+  state= {
+    isLoding: true
+  }
+
+  //비동기 처리방식
+  getLocation = async() => {
+    try {
+      //Location permission
+      await Location.requestPermissionsAsync();
+      //위치정보
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoding: false});
+      //위치정보 콘솔 확인
+      //console.log("죽지않아");
+    } catch(error) {
+      //에러 메세지
+      Alert.alert("읍스~~ 에러났어요.");
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+
+  render() {
+    const {isLoding} = this.state;
+    return isLoding ?<Loading /> : null;
+  }
+
 }
